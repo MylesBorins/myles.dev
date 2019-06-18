3a831197a3184f6ff4764ddadf7b1fdd1acb38fc
Myles Borins <myles.borins@gmail.com>
2019-06-18 00:52:24 -0400

    dynamic import for audio lib

--- public/index.mjs
+++ public/index.mjs
@@ -1,21 +1,20 @@
-import {
-  init,
-  stepOscillators
-} from './audio.mjs';
+let audio;
 
 const start = document.getElementById('start');
 let clicked = false;
 let lastY = 0;
 
-start.onclick = function onclick() {
+start.onclick = async function onclick() {
   if (clicked) return;
   clicked = true;
-  init();
+  audio = await import('./audio.mjs');
+  audio.init();
   start.setAttribute('style', 'display: none;')
 }
 
 document.body.onscroll = function onscroll() {
+  if (!audio) return;
   let diff = window.scrollY - lastY;
   lastY = window.scrollY;
-  stepOscillators(diff);
+  audio.stepOscillators(diff);
 }
